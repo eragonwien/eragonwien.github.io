e341c75dd915e10c9a26a30d48def31c1849594c
SonLaptop\erago <eragonwien@gmail.com>
2017-12-29 14:56:26 +0100

    fix typewriter overlapping

--- js/typewriter.js
+++ js/typewriter.js
@@ -1,37 +1,50 @@
-var i = 0; // charAt position
-var texts = ['beautiful and responsive UI.', 'mobile-friendly UX.', 'web application.'];
-var speed = 70; // wait 50 miliseconds between character
-var transition_speed = 1500; // wait 2 sec before erase the text
-setTimeout(typeWriter, 250, texts, 0);
-function typeWriter(arr, position) {
-    if (position >= arr.length) {
-        return;
-    }
-    var current = arr[position];
-    if (i < current.length) {
-        document.getElementById("textwriter").innerHTML += current.charAt(i);
-        i++;
-        setTimeout(typeWriter, speed, arr, position);
-    }
-    else {
-        if (position == arr.length - 1) {
+var timer = startWriting();
+
+function startWriting() {
+    var i = 0; // charAt position
+    var texts = ['beautiful and responsive UI.', 'mobile-friendly UX.', 'web application.'];
+    var speed = 50; // wait 50 miliseconds between character
+    var transition_speed = 1000; // wait 2 sec before erase the text
+    
+    timer = setTimeout(typeWriter, 0, texts, 0);
+    return timer;
+
+    function typeWriter(arr, position) {
+        if (position >= arr.length) {
             return;
         }
-        setTimeout(typeRemover, transition_speed, arr, position)
+        var current = arr[position];
+        if (i < current.length) {
+            document.getElementById("textwriter").innerHTML += current.charAt(i);
+            i++;
+            timer = setTimeout(typeWriter, speed, arr, position);
+        }
+        else {
+            if (position == arr.length - 1) {
+                return;
+            }
+            timer = setTimeout(typeRemover, transition_speed, arr, position)
+        }
     }
+    
+    function typeRemover(arr, position) {
+        if (position >= arr.length) {
+            return;
+        }
+        var current = arr[position];
+        if (i >= 0) {
+            document.getElementById("textwriter").innerHTML = current.slice(0, i);        
+            i--;
+            timer = setTimeout(typeRemover, speed, arr, position);
+        }
+        else {
+            timer = setTimeout(typeWriter, speed, arr, ++position)
+        }
+    }   
 }
 
-function typeRemover(arr, position) {
-    if (position >= arr.length) {
-        return;
-    }
-    var current = arr[position];
-    if (i >= 0) {
-        document.getElementById("textwriter").innerHTML = current.slice(0, i);        
-        i--;
-        setTimeout(typeRemover, speed, arr, position);
-    }
-    else {
-        setTimeout(typeWriter, speed, arr, ++position)
-    }
+function stopWriting() {
+    clearTimeout(timer);
 }
+
+
