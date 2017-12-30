b33e84ff4c016550f23f692493486dbc29a61a6e
SonLaptop\erago <eragonwien@gmail.com>
2017-12-30 05:18:48 +0100

    add portfolio page, add routing

--- js/ngapp.js
+++ js/ngapp.js
@@ -1,1 +1,35 @@
 var app = angular.module("eragonwien", ['ngRoute']);
+
+app.config(function($routeProvider){
+    $routeProvider
+    .when("/", {
+        title: "Start - Nguyen",
+        templateUrl: "overview.html"
+    })
+    .when("/portfolio", {
+        title: "Portfolio - Nguyen",            
+        templateUrl: "portfolio.html"
+    })
+    .when("/resume", {
+        title: "Resume - Nguyen",
+        templateUrl: "resume.html"
+    })
+    .when("/contact", {
+        title: "contact - Nguyen",
+        templateUrl: "contact.html"
+    })
+    .otherwise({
+        redirectTo: "/"
+    });
+});
+app.run(['$rootScope', '$route', function($rootScope, $route) {
+    $rootScope.$on('$routeChangeSuccess', function() {
+        document.title = $route.current.title;
+        scrollTop();
+    });
+}]);
+
+function scrollTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
---
+++ js/ngcontroller.js
@@ -0,0 +1,5 @@
+angular
+    .module('eragonwien')
+    .controller('maincontroller',function($scope){
+    
+    });
