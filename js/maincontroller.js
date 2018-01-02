9aa2835ba2665a8f0bb4c5e5e9e6cec4dd83d459
SonLaptop\erago <eragonwien@gmail.com>
2018-01-02 09:29:56 +0100

    add close button to contact modal

--- js/maincontroller.js
+++ js/maincontroller.js
@@ -3,9 +3,13 @@ var app = angular.module('eragonwien');
 app.controller('maincontroller', ['$scope', 'mainservice', function (scope, mainservice) {
     // INIT VARIABLES
     scope.contactPressed = false;
+    scope.contact = {};
 
     // ENABLE/ DIABLE CONTACT DIALOG
     scope.switchContact = function () {
         scope.contactPressed = !(scope.contactPressed);
+        if (!scope.contactPressed) {
+            scope.contact = {};
+        }
     }
 }]);
